c1e092b3a1620ea4c8aa0f35b8c072c2a901842b
ChonJi <remigiusz.chomicki@gmail.com>
2018-03-20 22:44:25 +0100

    Adding some chaining methods to achieve more clean code approach.

--- src/test/java/PageObject/HomePage.java
+++ src/test/java/PageObject/HomePage.java
@@ -1,9 +1,10 @@
 package PageObject;
 
 import com.codeborne.selenide.SelenideElement;
-import org.testng.AssertJUnit;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class HomePage {
 
@@ -14,45 +15,66 @@ public class HomePage {
     private SelenideElement targetField = $("input[name='target']");
     private SelenideElement optionField = $("input[name='aux']");
 
+    public static HomePage chainHomePageMethods() {
+        return new HomePage();
+    }
+
     /**
      * Checks header
      */
-    public void checkHeader(String headerText) {
-        AssertJUnit.assertEquals(header.getText(), headerText);
+    public HomePage checkHeader(String headerText) {
+        assertEquals(header.getText(), headerText);
+        return this;
     }
 
     /**
      * Checks if Reset Button is displayed
+     *
      * @return true if displayed
      */
-    public boolean isReserButtonDisplayed() {
-        return resetButton.isDisplayed();
+    public HomePage isResetButtonDisplayed() {
+        assertTrue(resetButton.isDisplayed());
+        return this;
     }
 
     /**
      * Checks if Submit Button is displayed
+     *
      * @return true if displayed
      */
-    public boolean isSubmitButtonDisplayed() {
-        return submitButton.isDisplayed();
+    public HomePage isSubmitButtonDisplayed() {
+        assertTrue(submitButton.isDisplayed());
+        return this;
     }
 
     /**
      * Checks if Action Field is displayed
+     *
      * @return true if displayed
      */
-    public boolean isActionFieldDisplayed() { return actionField.isDisplayed(); }
+    public HomePage isActionFieldDisplayed() {
+        assertTrue(actionField.isDisplayed());
+        return this;
+    }
 
     /**
      * Checks if Target Field is displayed
+     *
      * @return true if displayed
      */
-    public boolean isTargetFieldDisplayed() { return targetField.isDisplayed(); }
+    public HomePage isTargetFieldDisplayed() {
+        assertTrue(targetField.isDisplayed());
+        return this;
+    }
 
     /**
      * Checks if Option Field is displayed
+     *
      * @return true if displayed
      */
-    public boolean isOptionFieldDisplayed() { return optionField.isDisplayed(); }
+    public HomePage isOptionFieldDisplayed() {
+        assertTrue(optionField.isDisplayed());
+        return this;
+    }
 
 }
--- src/test/java/Tests/HomePageTest.java
+++ src/test/java/Tests/HomePageTest.java
@@ -1,33 +1,27 @@
 package Tests;
 
-import PageObject.HomePage;
 import PageObject.SetUp;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static PageObject.HomePage.chainHomePageMethods;
 import static TemporaryDataProvider.TemporaryDataProvider.headerText;
 import static com.codeborne.selenide.Selenide.close;
 
 
 public class HomePageTest extends SetUp {
 
-    private HomePage homePage;
-
     @BeforeClass
     private void setUp() {
-        homePage = openHomePage();
+        openHomePage();
     }
 
     @Test
     private void shouldOpenHomePage() {
-        homePage.checkHeader(headerText);
-        homePage.isReserButtonDisplayed();
-        homePage.isSubmitButtonDisplayed();
-        homePage.isActionFieldDisplayed();
-        homePage.isTargetFieldDisplayed();
-        homePage.isOptionFieldDisplayed();
+        chainHomePageMethods().checkHeader(headerText).isResetButtonDisplayed().isSubmitButtonDisplayed().isActionFieldDisplayed().isTargetFieldDisplayed().isOptionFieldDisplayed();
     }
+
 
     @AfterClass
     private void tearDown() {
